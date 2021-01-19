47fe536d6d54d8eb33847d1aa5f690392cdc43ca
Redfoxtrot279 <redfoxtrot279@gmail.com>
2021-01-19 14:22:03 -0600

    added 3 questions to script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+console.log(choices);
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -10,6 +11,27 @@ let availableQuestions = [];
 let questions = [
     {
         question: 'Inside which HTML element do we put the JavaScript??',
+        choice1: "<script>",
+        choice2: "<javascript>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer: 1
         
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        choice1: "<script href='xxx.js'>",
+        choice2: "<script name='xxx.js'>",
+        choice3: "<script src='xxx.js'>",
+        choice4: "<script file='xxx.js'>",
+        answer: 3
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World');",
+        choice3: "msg('Hello World');",
+        choice4: "alert('Hellow World');",
+        answer: 4
     }
-]
+];
