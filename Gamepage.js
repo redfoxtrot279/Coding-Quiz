e3d4c67655ae3e12267aabea9f4dd7ba42a7275a
Redfoxtrot279 <redfoxtrot279@gmail.com>
2021-01-19 15:55:19 -0600

    renamed files to stray from confusion

--- Gamepage.js
+++ Gamepage.js
@@ -1,6 +1,5 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-console.log(choices);
 
 let currentQuestion = {};
 let acceptingAnswers = false;
