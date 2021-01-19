1de86ac9f188c5e3c90ee59ceb722eb0b2db3f85
Redfoxtrot279 <redfoxtrot279@gmail.com>
2021-01-19 14:09:18 -0600

    fixed HTML classes

---
+++ script.js
@@ -0,0 +1,15 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript??',
+        
+    }
+]
